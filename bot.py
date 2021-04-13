e72b87904d4ad977f2812bee7695196a392f480a
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-04-13 14:55:25 +0530

    fixes gdrive issues

--- bot.py
+++ bot.py
@@ -34,8 +34,6 @@ def reset_url_status():
         url[i][1] = False
     print("-->RAN RESET FUNC....<----")
 
-        
-
 def schedulling_fun():               
 
         for i in range(len(url)):        
@@ -68,9 +66,14 @@ def schedulling_fun():
                                 #checking condition for the hindu....
                             if i == 0:
                                 all_links = soup.select(".entry-content.mh-clearfix p a")
-
                                 nxtpglink = all_links[4].get('href')
-                                # print(nxtpglink)
+                                print(nxtpglink)
+
+                                if nxtpglink.find("drive") > 5:     #drive link checking...
+                                    bot.send_message(chat_id = chat_id, text = "<b>The HINDU G-drive link:\tJoin:\t@allepaperadda</b>\n"+ nxtpglink, parse_mode = ParseMode.HTML )
+                                    print(' Uploaded!...OK')
+                                    url[i][1] = True
+                                         #skip the vk.com link as drivelink present....
                             else:      #else other epaper will come here
 
                                 all_links = soup.select(".entry-content p span a")
@@ -83,69 +86,70 @@ def schedulling_fun():
                             time.sleep(random.randint(5,12))
 
                             #moving to next page....
+                            if nxtpglink.find("vk.com") > 5: # verifying next pg vk.com link...
+                                if nxtpglink.find("?") > 5:  #checking for updated link...
+                                        res = requests.get(nxtpglink, headers = headers[rand_heads])
+                                            
+                                        soup = BeautifulSoup(res.text,'html.parser')
+                                        links = soup.find_all("iframe")
+                                        
+                                        for link in links:
+                                                
+                                                
+                                            full_dwnldlink = link['src']
+                                            dwnldlink = link['src'].split("?")
+                                            
+                                            ppr_name = dwnldlink[0].split("/")
+                                                
+
+                                            ttl_siz = len(ppr_name)-1
+                                            ppr_name = ppr_name[ttl_siz]
+                                                # print("Captured Downldlink ---> ",full_dwnldlink)
+                                            print(ppr_name)
+                                                
+
+
+                                        today_dt = datetime.datetime.now()
+                                        today_dt = today_dt.strftime("%d")
+                                        if today_dt[0]=="0":
+                                            today_dt = today_dt.replace("0","")
+                                                
+
+                                            #checking with todays date with the uploaded date
+                                        if int((ppr_name.replace("2021","").find(today_dt))) > 1 :
+                                                    #downloading...
+                                                
+                                                # res = requests.get(full_dwnldlink, stream = True)
+                                                # open('myfile/paper.pdf', 'wb').write(res.content)
+                                        
+
+                                                msg = '<b>'+ ppr_name +"\n\nJOIN NOW:\t @allepaperadda\nOr Search:\t\tallepaperadda\n\n" +'</b>'+ full_dwnldlink
+                                                print('Downloaded...OK')
+                                                
+                                                # time.sleep(2)
+                                                    #uploading to telegram channel...
+                                                
+                                                # bot.send_document(chat_id = chat_id, document = open('myfile/paper.pdf', 'rb'), caption = ppr_name +'@mychannel_link')
+                                                
+                                                bot.send_message(chat_id = chat_id, text = msg, parse_mode = ParseMode.HTML )
+                                                print('Uploaded!...OK')
+                                                url[i][1] = True
+                        
+                                                
+                                                time.sleep(random.randint(2,4)) #delay between next page which to be scrappped
+
+
+                                        else :
+                                                print("Not uploaded yet....last Epaper was: ", ppr_name)
+
                             
-                            if nxtpglink.find("?") > 5:  #checking for updated link...
-                                    res = requests.get(nxtpglink, headers =headers[rand_heads])
-                                    
-                                    soup = BeautifulSoup(res.text,'html.parser')
-                                    links = soup.find_all("iframe")
+                            elif nxtpglink.find("drive") > 5 :
+                                    print("Hindu gdrive link posted!")
                                 
-                                    for link in links:
-                                        
-                                        
-                                        full_dwnldlink = link['src']
-                                        dwnldlink = link['src'].split("?")
-                                    
-                                        ppr_name = dwnldlink[0].split("/")
-                                        
-
-                                        ttl_siz = len(ppr_name)-1
-                                        ppr_name = ppr_name[ttl_siz]
-                                        # print("Captured Downldlink ---> ",full_dwnldlink)
-                                        print(ppr_name)
-                                        
-
-
-                                    today_dt = datetime.datetime.now()
-                                    today_dt = today_dt.strftime("%d")
-                                    if today_dt[0]=="0":
-                                       today_dt = today_dt.replace("0","")
-                                        
-                                    
-                                    
-                                
-                                    #checking with todays date with the uploaded date
-                                    if int((ppr_name.replace("2021","").find(today_dt))) > 1 :
-                                            #downloading...
-                                        
-                                        # res = requests.get(full_dwnldlink, stream = True)
-                                        # open('myfile/paper.pdf', 'wb').write(res.content)
-                                
-
-                                        msg = '<b>'+ ppr_name +"\n\nJOIN NOW:\t @allepaperadda\nOr Search:\t\tallepaperadda\n\n" +'</b>'+ full_dwnldlink
-                                        print('Downloaded...OK')
-                                        
-                                        # time.sleep(2)
-                                            #uploading to telegram channel...
-                                        
-                                        # bot.send_document(chat_id = chat_id, document = open('myfile/paper.pdf', 'rb'), caption = ppr_name +'@mychannel_link')
-                                        
-                                        bot.send_message(chat_id = chat_id, text = msg, parse_mode = ParseMode.HTML )
-                                        print(' Uploaded!...OK')
-                                        url[i][1] = True
-                
-
-
-                                    else :
-                                        print("Not uploaded yet....last Epaper was: ", ppr_name)
-
-                                    #delay between next page which to be scrappped
+                                                
                             else:
                                 print("Something error happend in links...")
-                            
-                            time.sleep(random.randint(2,4))
-
-
+                           
 
             
                         else:
@@ -165,11 +169,11 @@ schedule.every().day.at("02:50").do(schedulling_fun)  #ist 08:20
 
 while True:
   
-    # Checks whether a scheduled task... 
-    #... is pending to run or not
     schedule.run_pending()
     time.sleep(1)            
 
+
+# schedulling_fun()   #for debugging USE THIS FUNCTION   
 
 updater = Updater(TOKEN)  
 updater.start_polling()
